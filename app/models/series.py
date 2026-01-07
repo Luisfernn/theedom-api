0f40b9ef64fcda4d932434e314cfc8f5560976b9
Luis Fernando <luisfernandowubi@gmail.com>
2026-01-07 16:13:18 -0300

    Removendo espaços desnecessários.

--- app/models/series.py
+++ app/models/series.py
@@ -7,9 +7,7 @@ class Series(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False)
     country = Column(String, nullable=False)
-
     status = Column(String, nullable=True)
     production_company = Column(String, nullable=True)
-
     date_start = Column(String, nullable=True)      
     date_watched = Column(String, nullable=True)
