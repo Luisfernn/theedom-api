2f6018dd8a67c9a8642ff1b89cc581bb248b5039
Luis Fernando <luisfernandowubi@gmail.com>
2026-01-10 09:38:48 -0300

    Importando os models series, ship_characters e ship_characters_series no service ship_characters.

--- app/services/ship_characters_service.py
+++ app/services/ship_characters_service.py
@@ -1,1 +1,5 @@
 from sqlalchemy.orm import Session
+
+from app.models.series import Series
+from app.models.ship_characters import ShipCharacter
+from app.models.ship_characters_series import ShipCharacterSeries
